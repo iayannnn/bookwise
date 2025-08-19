f3f5f3aa9637eb66d10258a150a1a52b7fbe2a26
iayannnn <farazayanali@gmail.com>
2025-08-19 11:40:41 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,25 +8,25 @@ const Footer = () => {
     <footer className="py-2 px-4 bg-slate-100 z-30 sm:flex items-center gap-2 border-t border-slate-200  sm:bg-white dark:bg-zinc-950 dark:border-zinc-800">
       
       <div className="sm:ml-auto flex items-center justify-center gap-2 ">
-        <Link to="https://www.linkedin.com/in/npharshavardhan/" target="_blank">
+        <Link to="https://www.linkedin.com/in/iayannnn/" target="_blank">
           <Button variant="outline" size="icon">
             <FaLinkedinIn className="h-6 w-6" />
             <span className="sr-only">Linkedin</span>
           </Button>
         </Link>
-        <Link to="https://github.com/harsha0502" target="_blank">
+        <Link to="https://github.com/iayannnn" target="_blank">
           <Button variant="outline" size="icon">
             <FaGithub className="h-6 w-6" />
             <span className="sr-only">Github</span>
           </Button>
         </Link>
-        <Link to="https://leetcode.com/u/_harsha_0502/" target="_blank">
+        <Link to="https://leetcode.com/u/iayannnn/" target="_blank">
           <Button variant="outline" size="icon">
             <SiLeetcode className="h-6 w-6" />
             <span className="sr-only">Leetcode</span>
           </Button>
         </Link>
-        <Link to="https://www.instagram.com/_harsha_0502/" target="_blank">
+        <Link to="https://www.instagram.com/iayannnn/" target="_blank">
           <Button variant="outline" size="icon">
             <FaInstagram className="h-6 w-6" />
             <span className="sr-only">Instagram</span>
